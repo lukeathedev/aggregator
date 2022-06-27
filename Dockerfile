3ed3a290502ea25884070fdb5e93f4ef2354530c
lal <alvarengalucas55@gmail.com>
2022-06-27 01:14:34 -0300

    Add Graceful Shutdown + Production Build

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,26 @@
-# TODO: Create deployment
+# Installing
+FROM node:16-alpine as build
+
+WORKDIR /usr/src/aggregator
+
+COPY package*.json ./
+COPY tsconfig.json ./
+
+COPY src ./src
+
+RUN npm install
+RUN npm run build
+
+# Running
+FROM node:16-alpine
+
+WORKDIR /usr/bin/aggregator
+VOLUME ./downloads
+
+COPY package*.json ./
+COPY .env .
+
+RUN npm ci --omit=dev
+COPY --from=build /usr/src/aggregator/build ./
+
+CMD ["node", "index.js"]
--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,22 @@ import express from "express";
 import xls2sql from "./xls2sql";
 import { Pool, Client } from "pg";
 
+const shutdown = () => {
+  log.info("Shutting down...");
+  server.close((err) => {
+    process.exit(err ? 1 : 0);
+  });
+};
+
+process.on("SIGTERM", shutdown);
+process.on("SIGINT", shutdown);
+
 const pool = new Pool();
 
 const app = express();
 const host = String(process.env.HOST);
 const port = Number(process.env.PORT);
-app.listen(port, host, () => {
+const server = app.listen(port, host, () => {
   log.info(`Listening on ${host}:${port}`);
 });
 
@@ -38,7 +48,6 @@ app.get("/update", async (req, res) => {
 
   pool.query(sql, (err, res) => {
     log.info(err, res);
-  })
+  });
   res.send(fn);
 });
-
